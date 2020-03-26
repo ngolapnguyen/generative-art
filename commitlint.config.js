5a950f9fbc503c7518ee7362129d32f2d89ec71a
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2020-03-26 10:58:39 +0700

    test: test & update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["feature", "fix", "test"]],
-        "references-empty": [2, "never"]
-    },
-    parserPreset: {
-        parserOpts: {
-            issuePrefixes: ["GA-"]
-        }
+        "type-enum": [2, "always", ["feature", "fix", "test"]]
     }
 };
